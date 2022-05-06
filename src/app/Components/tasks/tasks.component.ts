2d7c8b9b1572fada52a5eca51f3faa130c95fda8
Paul Joshua Premacio <pauljoshuapremacio@gmail.com>
2022-05-06 20:18:12 +0800

    first commit

---
+++ src/app/Components/tasks/tasks.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {Task} from '../../Task';
+import {TaskService} from '../../Services/task.service'
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[]  = [];
+  constructor(private taskService: TaskService) {
+
+  }
+
+  ngOnInit(): void {
+    this.taskService.getTask().subscribe((tasks) => this.tasks = tasks);
+  }
+  deleteTask(task: Task){
+    this.taskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter((t) => t.id !== task.id)));
+  }
+  toggleReminder(task: Task){
+    task.reminder = !task.reminder; 
+    this.taskService.updateTaskReminder(task).subscribe()
+  }
+  addTask(task: Task){
+    this.taskService.addTaskService(task).subscribe((tasks) => (this.tasks.push(task)));
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule, Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { ButtonComponent } from './Components/button/button.component';
+import { TasksComponent } from './Components/tasks/tasks.component';
+import { TaskItemComponent } from './Components/task-item/task-item.component';
+import { AddTaskComponent } from './Components/add-task/add-task.component';
+import { AboutComponent } from './Components/about/about.component';
+import { FooterComponent } from './Components/footer/footer.component';
+
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent},
+
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
